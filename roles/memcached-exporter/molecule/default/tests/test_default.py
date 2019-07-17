d6efbdbbe92571ce14799a21136a8f7cff921f55
kong62 <kong62@outlook.com>
2019-07-17 14:31:41 +0800

    add exporter

---
+++ roles/memcached-exporter/molecule/default/tests/test_default.py
@@ -0,0 +1,20 @@
+import os
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+def test_hosts_file(host):
+    f = host.file('/etc/hosts')
+
+    assert f.exists
+    assert f.user == 'root'
+    assert f.group == 'root'
+
+
+def test_memcached_exporter_is_running(host):
+    f = host.service('prometheus-memcached-exporter')
+
+    assert f.is_running
